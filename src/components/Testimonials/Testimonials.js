6bdd9b3f78d0430951e4d44724a8ad8e3c149f98
sakhawat9 <sakhawathossain7969@gmail.com>
2022-02-07 16:25:21 +0600

    edit testimonials

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,7 +1,8 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { AiOutlineStar } from "react-icons/ai";
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+import { IoMdQuote } from "react-icons/io";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
@@ -86,41 +87,72 @@ const Testimonials = ({ data }) => {
 
 const Card = ({ item }) => {
   return (
-    <div className="p-6 m-3 shadow-lg">
-      <div className="grid items-center grid-cols-12 gap-4 mb-4">
-        <div className="col-span-8">
-          <h3 className="pt-3 text-2xl">{item.name}</h3>
-          <ul className="flex mt-3">
-            <li>
-              <AiOutlineStar className="text-2xl text-yellow-400" />
-            </li>
-            <li>
-              <AiOutlineStar className="text-2xl text-yellow-400" />
-            </li>
-            <li>
-              <AiOutlineStar className="text-2xl text-yellow-400" />
-            </li>
-            <li>
-              <AiOutlineStar className="text-2xl text-yellow-400" />
-            </li>
-            <li>
-              <AiOutlineStar className="text-2xl text-yellow-400" />
-            </li>
-          </ul>
-        </div>
-        <div className="col-span-4">
-          <div className="inline-flex p-2 align-middle bg-white rounded-full shadow">
-            <Image
-              width="70"
-              height="70"
-              className="object-cover rounded-full"
-              src={item.img}
-              alt={item.name}
-            />
-          </div>
-        </div>
+    // <div className="p-6 m-3 shadow-lg">
+    //   <div className="grid items-center grid-cols-12 gap-4 mb-4">
+    //     <div className="col-span-8">
+    //       <h3 className="pt-3 text-2xl">{item.name}</h3>
+    //       <ul className="flex mt-3">
+    //         <li>
+    //           <AiOutlineStar className="text-2xl text-yellow-400" />
+    //         </li>
+    //         <li>
+    //           <AiOutlineStar className="text-2xl text-yellow-400" />
+    //         </li>
+    //         <li>
+    //           <AiOutlineStar className="text-2xl text-yellow-400" />
+    //         </li>
+    //         <li>
+    //           <AiOutlineStar className="text-2xl text-yellow-400" />
+    //         </li>
+    //         <li>
+    //           <AiOutlineStar className="text-2xl text-yellow-400" />
+    //         </li>
+    //       </ul>
+    //     </div>
+    //     <div className="col-span-4">
+    //       <div className="inline-flex p-2 align-middle bg-white rounded-full shadow">
+    //         <Image
+    //           width="70"
+    //           height="70"
+    //           className="object-cover rounded-full"
+    //           src={item.img}
+    //           alt={item.name}
+    //         />
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <p className="text-sm text-justify">{item.description}</p>
+    // </div>
+    <div className="single-review-item">
+      <ul className="single-review-item__ratting">
+        <li>
+          <AiOutlineStar />
+        </li>
+        <li>
+          <AiOutlineStar />
+        </li>
+        <li>
+          <AiOutlineStar />
+        </li>
+        <li>
+          <AiOutlineStar />
+        </li>
+        <li>
+          <AiOutlineStar />
+        </li>
+      </ul>
+      <p>{item.description}</p>
+      <div className="single-review-item__info">
+        <img src={item.img} alt="image" />
+        <h3>{item.name}</h3>
       </div>
-      <p className="text-sm text-justify">{item.description}</p>
+      <div className="single-review-item__quote-icon">
+        <ul>
+          <li>
+            <IoMdQuote />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
